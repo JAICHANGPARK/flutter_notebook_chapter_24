26ee01c7d762bfad4a170fd288c28cce22e1f75f
Dreamwalker <aristojeff@gmail.com>
2022-11-30 17:45:31 +0900

    Create cargo_shipping_main_page.dart

---
+++ lib/ep1351_cargo_shipping_app/view/cargo_shipping_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CargoShippingMainPage extends StatefulWidget {
+  const CargoShippingMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<CargoShippingMainPage> createState() => _CargoShippingMainPageState();
+}
+
+class _CargoShippingMainPageState extends State<CargoShippingMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

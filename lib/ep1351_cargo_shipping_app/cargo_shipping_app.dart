92baaec8d42f8de5f24a10a3b4a86cbe407ee65a
Dreamwalker <aristojeff@gmail.com>
2022-11-30 17:45:35 +0900

    Update cargo_shipping_app.dart

--- lib/ep1351_cargo_shipping_app/cargo_shipping_app.dart
+++ lib/ep1351_cargo_shipping_app/cargo_shipping_app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_24/ep1351_cargo_shipping_app/view/cargo_shipping_main_page.dart';
 
 class CargoShippingApp extends StatelessWidget {
   const CargoShippingApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: CargoShippingMainPage(),
+    );
   }
 }

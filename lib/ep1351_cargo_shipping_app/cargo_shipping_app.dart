3f9a4da3ebe738dba30197b401bde054e40a35cc
Dreamwalker <aristojeff@gmail.com>
2022-11-30 17:44:00 +0900

    Create cargo_shipping_app.dart

---
+++ lib/ep1351_cargo_shipping_app/cargo_shipping_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class CargoShippingApp extends StatelessWidget {
+  const CargoShippingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

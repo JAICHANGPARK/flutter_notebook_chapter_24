843d32a7b54f2950c12ac4bd50bd4e9bc505a762
Dreamwalker <aristojeff@gmail.com>
2022-11-30 17:45:38 +0900

    Update ep1351_main.dart

---
+++ lib/ep1351_cargo_shipping_app/ep1351_main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_24/ep1351_cargo_shipping_app/cargo_shipping_app.dart';
+
+main() {
+  runApp(const CargoShippingApp());
+}

cc8dcc6fa6c16e653c53d58f53ed932445fccd0d
Dreamwalker <aristojeff@gmail.com>
2022-12-03 11:54:15 +0900

    Create rep_home_screen.dart

---
+++ lib/ep1354_real_estate_property_app/view/rep_home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RealEstatePropertyHomeScreen extends StatefulWidget {
+  const RealEstatePropertyHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RealEstatePropertyHomeScreen> createState() => _RealEstatePropertyHomeScreenState();
+}
+
+class _RealEstatePropertyHomeScreenState extends State<RealEstatePropertyHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+
+    );
+  }
+}

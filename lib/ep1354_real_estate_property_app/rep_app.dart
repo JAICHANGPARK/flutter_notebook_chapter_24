63377e46f88da3e65ccce70964f53eaa0f6de6dc
Dreamwalker <aristojeff@gmail.com>
2022-12-03 11:54:47 +0900

    Update rep_app.dart

--- lib/ep1354_real_estate_property_app/rep_app.dart
+++ lib/ep1354_real_estate_property_app/rep_app.dart
@@ -1,12 +1,13 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_24/ep1354_real_estate_property_app/view/rep_home_screen.dart';
 
 class RealEstatePropertyApp extends StatelessWidget {
   const RealEstatePropertyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      home: RealEstatePropertyHomeScreen(),
+    );
   }
 }

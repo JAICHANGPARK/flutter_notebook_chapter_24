c036be6eec0a8ecb99e110695161cf29937583eb
Dreamwalker <aristojeff@gmail.com>
2022-12-05 10:09:47 +0900

    Update ep1354_main.dart

--- lib/ep1354_real_estate_property_app/ep1354_main.dart
+++ lib/ep1354_real_estate_property_app/ep1354_main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1354_real_estate_property_app/rep_app.dart';
 
-void main() => runApp(RealEstatePropertyApp());
+void main() => runApp(const RealEstatePropertyApp());

428d8ebc97602abcf023a62cffdaec45c1623c95
Dreamwalker <aristojeff@gmail.com>
2022-12-05 10:00:52 +0900

    Create rep_detail_screen.dart

---
+++ lib/ep1354_real_estate_property_app/view/rep_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RealEstateDetailScreen extends StatefulWidget {
+  const RealEstateDetailScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RealEstateDetailScreen> createState() => _RealEstateDetailScreenState();
+}
+
+class _RealEstateDetailScreenState extends State<RealEstateDetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

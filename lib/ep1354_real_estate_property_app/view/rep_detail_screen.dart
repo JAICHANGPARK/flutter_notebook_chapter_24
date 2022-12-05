75ea67360de6356e6dfe3dd4ffa3e74b2c3e4e3d
Dreamwalker <aristojeff@gmail.com>
2022-12-05 10:16:58 +0900

    Update rep_detail_screen.dart

--- lib/ep1354_real_estate_property_app/view/rep_detail_screen.dart
+++ lib/ep1354_real_estate_property_app/view/rep_detail_screen.dart
@@ -11,7 +11,22 @@ class _RealEstateDetailScreenState extends State<RealEstateDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(onPressed: () {}, icon: const Icon(Icons.favorite)),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.forward)),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.send)),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.bookmark)),
+                Spacer(),
+                IconButton(onPressed: () {}, icon: Icon(Icons.cancel))
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

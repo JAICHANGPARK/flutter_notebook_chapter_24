83d7877af9796444c0151abfefd72a61c00eb344
Dreamwalker <aristojeff@gmail.com>
2022-12-06 17:35:11 +0900

    Create ep1357_main.dart

---
+++ lib/ep1357_crypto_wallet_app/ep1357_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(const CryptoWalletApp());
+}
+
+class CryptoWalletApp extends StatelessWidget {
+  const CryptoWalletApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

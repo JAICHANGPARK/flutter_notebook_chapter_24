4f284367c9cee506b6ce7973d42e0ae1392d8e2e
Dreamwalker <aristojeff@gmail.com>
2022-12-06 17:42:06 +0900

    Create cwa_main_page.dart

---
+++ lib/ep1357_crypto_wallet_app/view/cwa_main_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+class CryptoWalletAppMainPage extends StatefulWidget {
+  const CryptoWalletAppMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<CryptoWalletAppMainPage> createState() => _CryptoWalletAppMainPageState();
+}
+
+class _CryptoWalletAppMainPageState extends State<CryptoWalletAppMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.menu,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Ionicons.notifications_outline,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Ionicons.search,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

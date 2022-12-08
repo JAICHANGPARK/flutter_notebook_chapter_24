1b3ff0b1c0d92119f54557406411468055c309d8
Dreamwalker <aristojeff@gmail.com>
2022-12-08 17:14:02 +0900

    Update ep1357_main.dart

--- lib/ep1357_crypto_wallet_app/ep1357_main.dart
+++ lib/ep1357_crypto_wallet_app/ep1357_main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1357_crypto_wallet_app/view/cwa_main_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
-  runApp(const CryptoWalletApp());
+  runApp(ProviderScope(child: const CryptoWalletApp()));
 }
 
 class CryptoWalletApp extends StatelessWidget {

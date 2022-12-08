4097f9b54dd5fc796418c515525b690ddb755ee1
Dreamwalker <aristojeff@gmail.com>
2022-12-08 17:16:47 +0900

    Update cwa_main_page.dart

--- lib/ep1357_crypto_wallet_app/view/cwa_main_page.dart
+++ lib/ep1357_crypto_wallet_app/view/cwa_main_page.dart
@@ -1,5 +1,6 @@
 import 'package:crypto_font_icons/crypto_font_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_24/ep1357_crypto_wallet_app/view/cwa_bottom_bar_widget.dart';
 import 'package:ionicons/ionicons.dart';
 
 class CryptoWalletAppMainPage extends StatefulWidget {
@@ -223,7 +224,7 @@ class _CryptoWalletAppMainPageState extends State<CryptoWalletAppMainPage> {
                   "My Wallets",
                   style: TextStyle(fontSize: 24),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 16,
                 ),
                 Expanded(
@@ -242,7 +243,7 @@ class _CryptoWalletAppMainPageState extends State<CryptoWalletAppMainPage> {
                                 border: Border.all(),
                                 shape: BoxShape.circle,
                               ),
-                              child: Center(
+                              child: const Center(
                                 child: Icon(
                                   CryptoFontIcons.ETH,
                                 ),
@@ -259,20 +260,20 @@ class _CryptoWalletAppMainPageState extends State<CryptoWalletAppMainPage> {
                                   Row(
                                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text("Etherium"),
-                                      Text("ETh 9835.73"),
+                                      const Text("Etherium"),
+                                      const Text("ETh 9835.73"),
                                     ],
                                   ),
                                   Row(
                                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                     children: [
-                                      Text("ETH"),
+                                      const Text("ETH"),
                                       Row(
                                         children: [
-                                          Icon(
+                                          const Icon(
                                             Icons.arrow_drop_up,
                                           ),
-                                          Text("2.28%"),
+                                          const Text("2.28%"),
                                         ],
                                       )
                                     ],
@@ -289,80 +290,11 @@ class _CryptoWalletAppMainPageState extends State<CryptoWalletAppMainPage> {
               ],
             ),
           ),
-          Positioned(
+          const Positioned(
             bottom: 16,
             left: 16,
             right: 16,
-            child: Container(
-              height: 100,
-              child: Stack(
-                children: [
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    top: 24,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: Colors.black,
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Ionicons.home_outline),
-                            color: Colors.white,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Ionicons.wallet_outline),
-                            color: Colors.grey,
-                          ),
-                          const SizedBox(
-                            width: 64,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Ionicons.card_outline),
-                            color: Colors.grey,
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Ionicons.person_outline),
-                            color: Colors.grey,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 16,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        color: Colors.black,
-                        shape: BoxShape.circle,
-                      ),
-                      child: const Center(
-                        child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          foregroundColor: Colors.black,
-                          child: Icon(
-                            Icons.import_export,
-                            size: 42,
-                          ),
-                          radius: 32,
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
+            child: CryptoWalletAppBottomBar(),
           )
         ],
       ),

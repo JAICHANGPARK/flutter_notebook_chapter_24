5235bcfdbd289449e8e3a79a0a91645ce156b296
Dreamwalker <aristojeff@gmail.com>
2022-12-08 17:20:22 +0900

    Create cwa_model.dart

---
+++ lib/ep1357_crypto_wallet_app/model/cwa_model.dart
@@ -0,0 +1,11 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'cwa_model.freezed.dart';
+
+@freezed
+class CwaModel with _$CwaModel {
+  factory CwaModel(
+
+
+      ) = _CwaModel;
+}

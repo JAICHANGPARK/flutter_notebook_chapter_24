28c9d63df3111ec7922d253f9bc2e134dcaf43a7
Dreamwalker <aristojeff@gmail.com>
2022-12-08 17:26:01 +0900

    Create caw_fake_data.dart

---
+++ lib/ep1357_crypto_wallet_app/model/caw_fake_data.dart
@@ -0,0 +1,9 @@
+import 'package:crypto_font_icons/crypto_font_icons.dart';
+import 'package:flutter_notebook_chapter_24/ep1357_crypto_wallet_app/model/cwa_model.dart';
+
+List<CwaModel> cwaFakeDataItems = [
+  CwaModel(CryptoFontIcons.ETH, "Etherium"),
+  CwaModel(CryptoFontIcons.BTC, "Bitcoin"),
+  CwaModel(CryptoFontIcons.DGD, "????"),
+  CwaModel(CryptoFontIcons.ETH, "Etherium"),
+];

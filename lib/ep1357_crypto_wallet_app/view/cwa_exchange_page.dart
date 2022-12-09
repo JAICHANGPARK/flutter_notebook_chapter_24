2bc0a7001f03ffe77704de4fd0e51144b44b5d7a
Dreamwalker <aristojeff@gmail.com>
2022-12-09 10:29:13 +0900

    Create cwa_exchange_page.dart

---
+++ lib/ep1357_crypto_wallet_app/view/cwa_exchange_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class CwaExchangePage extends StatefulWidget {
+  const CwaExchangePage({Key? key}) : super(key: key);
+
+  @override
+  State<CwaExchangePage> createState() => _CwaExchangePageState();
+}
+
+class _CwaExchangePageState extends State<CwaExchangePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

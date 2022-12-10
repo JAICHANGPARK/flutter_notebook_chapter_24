5328033f586c6e55cdd5110d51399a523cf3dd5f
Dreamwalker <aristojeff@gmail.com>
2022-12-10 11:44:34 +0900

    Create ep1361_main.dart

---
+++ lib/ep1361_home_assistant_app/ep1361_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(HomeAssistantApp());
+}
+
+class HomeAssistantApp extends StatelessWidget {
+  const HomeAssistantApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

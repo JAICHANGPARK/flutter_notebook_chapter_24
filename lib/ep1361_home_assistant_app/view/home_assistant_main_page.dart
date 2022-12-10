dbedf11074575322af28e7de11b332754d6a68b8
Dreamwalker <aristojeff@gmail.com>
2022-12-10 11:45:34 +0900

    Create home_assistant_main_page.dart

---
+++ lib/ep1361_home_assistant_app/view/home_assistant_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HomeAssistantMainPage extends StatefulWidget {
+  const HomeAssistantMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<HomeAssistantMainPage> createState() => _HomeAssistantMainPageState();
+}
+
+class _HomeAssistantMainPageState extends State<HomeAssistantMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

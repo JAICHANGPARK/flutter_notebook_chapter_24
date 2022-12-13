ad3b7f395006b59f5f138581e2d1dbb2f5b5b04f
Dreamwalker <aristojeff@gmail.com>
2022-12-13 16:58:29 +0900

    Update ep1365_main.dart

--- lib/ep1365_travel_app/ep1365_main.dart
+++ lib/ep1365_travel_app/ep1365_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_24/ep1365_travel_app/view/travel_app_main_page.dart';
 
 void main() {
   runApp(const TripifyTravelApp());
@@ -9,6 +10,8 @@ class TripifyTravelApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: TravelAppMainPage(),
+    );
   }
 }

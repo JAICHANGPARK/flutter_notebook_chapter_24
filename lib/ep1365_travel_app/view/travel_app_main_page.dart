fd37f91cb82f16ce961bbfd5b03667294c511bec
Dreamwalker <aristojeff@gmail.com>
2022-12-13 16:58:26 +0900

    Update travel_app_main_page.dart

---
+++ lib/ep1365_travel_app/view/travel_app_main_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class TravelAppMainPage extends StatefulWidget {
+  const TravelAppMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<TravelAppMainPage> createState() => _TravelAppMainPageState();
+}
+
+class _TravelAppMainPageState extends State<TravelAppMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}

f84c9067dd8c666569f6a13343149afaf8b685be
Dreamwalker <aristojeff@gmail.com>
2022-12-13 16:54:02 +0900

    Create ep1365_main.dart

---
+++ lib/ep1365_travel_app/ep1365_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TripifyTravelApp());
+}
+
+class TripifyTravelApp extends StatelessWidget {
+  const TripifyTravelApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

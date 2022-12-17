67bc9dfb4af272070b287bc9412fd40b14f73dd8
Dreamwalker <aristojeff@gmail.com>
2022-12-17 11:44:05 +0900

    Update fec_main_page.dart

--- lib/ep1368_fashion_e_commerce_app/view/fec_main_page.dart
+++ lib/ep1368_fashion_e_commerce_app/view/fec_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FashionECommerceMainPage extends StatefulWidget {
   const FashionECommerceMainPage({Key? key}) : super(key: key);
 
@@ -12,7 +11,19 @@ class _FashionECommerceMainPageState extends State<FashionECommerceMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+                left: 16,
+                right: 16,
+                top: 0,
+                child: Container(
+                  height: 64,
+                ))
+          ],
+        ),
+      ),
     );
   }
 }

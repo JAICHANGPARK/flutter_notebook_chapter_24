c8153339535fa528b714e27c8417e2dff5b92be0
Dreamwalker <aristojeff@gmail.com>
2022-12-17 11:41:38 +0900

    Update ep1368_main.dart

---
+++ lib/ep1368_fashion_e_commerce_app/ep1368_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FashionECommerceApp());
+}
+
+class FashionECommerceApp extends StatelessWidget {
+  const FashionECommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

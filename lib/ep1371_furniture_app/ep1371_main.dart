b948eb9dd41e3f19cea88970ce08c36e0cac756b
Dreamwalker <aristojeff@gmail.com>
2022-12-20 22:54:29 +0900

    Create ep1371_main.dart

---
+++ lib/ep1371_furniture_app/ep1371_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FurnitureApp());
+}
+
+class FurnitureApp extends StatelessWidget {
+  const FurnitureApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

ef196dae8bf1987f8361f7be2a3f2c7c37a811bd
Dreamwalker <aristojeff@gmail.com>
2022-12-21 16:40:16 +0900

    add index controller

---
+++ lib/ep1371_furniture_app/controller/funiture_app_controller.dart
@@ -0,0 +1,4 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final furnitureMenuIndex = StateProvider((ref) => 0);
--- lib/ep1371_furniture_app/ep1371_main.dart
+++ lib/ep1371_furniture_app/ep1371_main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1371_furniture_app/view/furniture_main_page.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const FurnitureApp());
+  runApp(ProviderScope(child: const FurnitureApp()));
 }
 
 class FurnitureApp extends StatelessWidget {

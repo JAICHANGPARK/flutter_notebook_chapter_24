f3c268016f11d378642e163e3efe7edcac3202c2
Dreamwalker <aristojeff@gmail.com>
2022-12-22 17:48:49 +0900

    Update furniture_main_page.dart

--- lib/ep1371_furniture_app/view/furniture_main_page.dart
+++ lib/ep1371_furniture_app/view/furniture_main_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1371_furniture_app/controller/funiture_app_controller.dart';
+import 'package:flutter_notebook_chapter_24/ep1371_furniture_app/view/furniture_detail_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class FurnitureMainPage extends StatefulWidget {
@@ -86,82 +87,94 @@ class _FurnitureMainPageState extends State<FurnitureMainPage> {
                           childAspectRatio: 0.7,
                         ),
                         itemBuilder: (context, index) {
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                height: 180,
-                                decoration: BoxDecoration(
-                                  color: Colors.grey,
-                                  borderRadius: BorderRadius.circular(8),
-                                  image: const DecorationImage(
-                                    image: NetworkImage(
-                                      "https://cdn.pixabay.com/photo/2016/11/21/12/59/couch-1845270_960_720.jpg",
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const FurnitureDetailPage(),
+                                ),
+                              );
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  height: 180,
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey,
+                                    borderRadius: BorderRadius.circular(8),
+                                    image: const DecorationImage(
+                                      image: NetworkImage(
+                                        "https://cdn.pixabay.com/photo/2016/11/21/12/59/couch-1845270_960_720.jpg",
+                                      ),
+                                      fit: BoxFit.cover,
                                     ),
-                                    fit: BoxFit.cover,
                                   ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 16,
-                              ),
-                              const Text("Cottages Sofa"),
-                              const SizedBox(
-                                height: 4,
-                              ),
-                              const Text("Stock: 21"),
-                              const SizedBox(
-                                height: 8,
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 24,
-                                    width: 24,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(4),
-                                      border: Border.all(
-                                        color: Colors.grey,
+                                const SizedBox(
+                                  height: 16,
+                                ),
+                                const Text("Cottages Sofa"),
+                                const SizedBox(
+                                  height: 4,
+                                ),
+                                const Text("Stock: 21"),
+                                const SizedBox(
+                                  height: 8,
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 24,
+                                      width: 24,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                        ),
+                                        color: Colors.white,
                                       ),
-                                      color: Colors.white,
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    width: 12,
-                                  ),
-                                  Container(
-                                    height: 24,
-                                    width: 24,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(4),
-                                      border: Border.all(
-                                        color: Colors.grey,
+                                    const SizedBox(
+                                      width: 12,
+                                    ),
+                                    Container(
+                                      height: 24,
+                                      width: 24,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                        ),
+                                        color: Colors.green,
                                       ),
-                                      color: Colors.green,
                                     ),
-                                  ),
-                                  const SizedBox(
-                                    width: 12,
-                                  ),
-                                  Container(
-                                    height: 24,
-                                    width: 24,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(4),
-                                      border: Border.all(
-                                        color: Colors.grey,
+                                    const SizedBox(
+                                      width: 12,
+                                    ),
+                                    Container(
+                                      height: 24,
+                                      width: 24,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(4),
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                        ),
+                                        color: Colors.orange,
                                       ),
-                                      color: Colors.orange,
                                     ),
-                                  ),
-                                  const Spacer(),
-                                  const Text(
-                                    "\$24",
-                                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-                                  )
-                                ],
-                              )
-                            ],
+                                    const Spacer(),
+                                    const Text(
+                                      "\$24",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
                           );
                         }),
                     Container(),

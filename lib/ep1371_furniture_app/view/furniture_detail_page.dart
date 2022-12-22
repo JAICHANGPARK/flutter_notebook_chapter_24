82c5c136d20a51cc555134b93499fee35062ba5c
Dreamwalker <aristojeff@gmail.com>
2022-12-22 17:53:53 +0900

    Update furniture_detail_page.dart

--- lib/ep1371_furniture_app/view/furniture_detail_page.dart
+++ lib/ep1371_furniture_app/view/furniture_detail_page.dart
@@ -18,7 +18,7 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(
+            icon: const Icon(
               Icons.favorite,
             ),
             color: Colors.red,
@@ -31,20 +31,64 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
             height: 300,
             color: Colors.blue,
           ),
-          Text("Cottages Sofa"),
-          Text("Stock: 21"),
+          const Text("Cottages Sofa"),
+          const Text("Stock: 21"),
           Row(
             children: [
               Container(
-                height: 38,
-                width: 38,
+                height: 42,
+                width: 42,
                 decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(8)
-                ),
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8)),
+              ),
+              Container(
+                height: 42,
+                width: 42,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8)),
+              ),
+              Container(
+                height: 42,
+                width: 42,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8)),
               )
             ],
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: const Text("Details"),
+          ),
+          Container(
+            height: 140,
+            decoration: BoxDecoration(
+              color: Colors.purple[200],
+            ),
+          ),
+          Container(
+            height: 54,
+            decoration: const ShapeDecoration(
+              shape: StadiumBorder(),
+              color: Colors.black,
+            ),
+            child: const Center(
+              child: Text(
+                "Add to Chart",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           )
         ],
       ),

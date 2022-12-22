358c45ce852247f70961ae385a6524a1a1fb0296
Dreamwalker <aristojeff@gmail.com>
2022-12-22 17:56:27 +0900

    Update furniture_detail_page.dart

--- lib/ep1371_furniture_app/view/furniture_detail_page.dart
+++ lib/ep1371_furniture_app/view/furniture_detail_page.dart
@@ -11,102 +11,104 @@ class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        foregroundColor: Colors.black,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.favorite,
-            ),
-            color: Colors.red,
-          ),
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body:
-      Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16 , vertical: 24),
-        child: Column(
-          children: [
-            Container(
-              height: 300,
-              color: Colors.blue,
-            ),
-          SizedBox(height: 24,),
-            const Text("Cottages Sofa", style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold
-            ),),
-            const Text("Stock: 21", style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold
-            ),),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8)),
-                ),
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8)),
-                ),
-                Container(
-                  height: 42,
-                  width: 42,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey),
-                      borderRadius: BorderRadius.circular(8)),
-                )
-              ],
-            ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius: BorderRadius.circular(24),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          foregroundColor: Colors.black,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.favorite,
               ),
-              padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              child: const Text("Details"),
+              color: Colors.red,
             ),
-            Container(
-              height: 140,
-              decoration: BoxDecoration(
-                color: Colors.purple[200],
-              ),
-            ),
-            Container(
-              height: 54,
-              decoration: const ShapeDecoration(
-                shape: StadiumBorder(),
-                color: Colors.black,
-              ),
-              child: const Center(
-                child: Text(
-                  "Add to Chart",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            )
           ],
         ),
-      )
-
-    );
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                color: Colors.blue,
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              const Text(
+                "Cottages Sofa",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                "Stock: 21",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 42,
+                      width: 42,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    Container(
+                      height: 42,
+                      width: 42,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    Container(
+                      height: 42,
+                      width: 42,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(color: Colors.grey),
+                          borderRadius: BorderRadius.circular(8)),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.circular(24),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                child: const Text("Details"),
+              ),
+              Container(
+                height: 140,
+                decoration: BoxDecoration(
+                  color: Colors.purple[200],
+                ),
+              ),
+              Container(
+                height: 54,
+                decoration: const ShapeDecoration(
+                  shape: StadiumBorder(),
+                  color: Colors.black,
+                ),
+                child: const Center(
+                  child: Text(
+                    "Add to Chart",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }

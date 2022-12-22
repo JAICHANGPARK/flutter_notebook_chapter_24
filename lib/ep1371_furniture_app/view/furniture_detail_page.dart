2f8b9cffb70650fade64bd518f9ab0bc230a4680
Dreamwalker <aristojeff@gmail.com>
2022-12-22 17:47:56 +0900

    Update furniture_detail_page.dart

--- lib/ep1371_furniture_app/view/furniture_detail_page.dart
+++ lib/ep1371_furniture_app/view/furniture_detail_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class FurnitureDetailPage extends StatefulWidget {
   const FurnitureDetailPage({Key? key}) : super(key: key);
 
@@ -11,6 +10,15 @@ class FurnitureDetailPage extends StatefulWidget {
 class _FurnitureDetailPageState extends State<FurnitureDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.favorite)),
+        ],
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
   }
 }

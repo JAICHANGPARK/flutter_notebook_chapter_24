858662e4584c3d789797f7d83947b4165c77f632
Dreamwalker <aristojeff@gmail.com>
2022-12-23 19:22:08 +0900

    Create news_app_main_apge.dart

---
+++ lib/ep1377_news_app/view/news_app_main_apge.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewsAppMainPage extends StatefulWidget {
+  const NewsAppMainPage({Key key}) : super(key: key);
+
+  @override
+  _NewsAppMainPageState createState() => _NewsAppMainPageState();
+}
+
+class _NewsAppMainPageState extends State<NewsAppMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}

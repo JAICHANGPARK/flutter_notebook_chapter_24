bfae7a83bd6d08ab88305887d35cc8a811262efe
Dreamwalker <aristojeff@gmail.com>
2022-12-23 19:26:08 +0900

    Update news_app_main_apge.dart

--- lib/ep1377_news_app/view/news_app_main_apge.dart
+++ lib/ep1377_news_app/view/news_app_main_apge.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NewsAppMainPage extends StatefulWidget {
-  const NewsAppMainPage({Key key}) : super(key: key);
+  const NewsAppMainPage({Key? key}) : super(key: key);
 
   @override
   _NewsAppMainPageState createState() => _NewsAppMainPageState();
@@ -11,7 +11,45 @@ class _NewsAppMainPageState extends State<NewsAppMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 0,
+            top: 16,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    CircleAvatar(),
+                    Column(
+                      children: [Text("Welcome back!"), Text("Dreamwalker")],
+                    ),
+                    Container(
+                      height: 64,
+                      width: 64,
+                      decoration: BoxDecoration(
+                        border: Border.all(),
+                        shape: BoxShape.circle,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            child: Container(
+              height: 64,
+              color: Colors.black,
+            ),
+            bottom: 16,
+            left: 24,
+            right: 24,
+          )
+        ],
+      ),
     );
   }
 }

8cf1eed7a7756a7662b5339b0495a918fd21ea96
Dreamwalker <aristojeff@gmail.com>
2022-12-23 13:00:23 +0900

    Create fake_furniture_data.dart

---
+++ lib/ep1371_furniture_app/data/fake_furniture_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_notebook_chapter_24/ep1371_furniture_app/model/furniture_item.dart';
+
+List<FurnitureItem> fakeFurnitureItems = [
+  FurnitureItem(
+    title: "Dreamwalker Sofa",
+    stock: "21",
+  )
+];

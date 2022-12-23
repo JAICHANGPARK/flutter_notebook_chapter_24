55a5f74a0597bcf815b8b2193b4ce93b27cbdee3
Dreamwalker <aristojeff@gmail.com>
2022-12-23 13:02:16 +0900

    Update fake_furniture_data.dart

--- lib/ep1371_furniture_app/data/fake_furniture_data.dart
+++ lib/ep1371_furniture_app/data/fake_furniture_data.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1371_furniture_app/model/furniture_item.dart';
 
 List<FurnitureItem> fakeFurnitureItems = [
   FurnitureItem(
     title: "Dreamwalker Sofa",
     stock: "21",
+    colors: [
+      Colors.white,
+      Colors.green,
+    ],
+    price: "24",
+  ),
+  FurnitureItem(
+    title: "Cottages Sofa",
+    stock: "21",
+    colors: [Colors.white, Colors.green, Colors.orange],
+    price: "24",
   )
 ];

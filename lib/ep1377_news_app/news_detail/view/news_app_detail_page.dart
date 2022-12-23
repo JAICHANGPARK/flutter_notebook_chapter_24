1626550c66516ab64b9979cdf7c8b8b4d781e4fa
Dreamwalker <aristojeff@gmail.com>
2022-12-23 21:52:20 +0900

    Update news_app_detail_page.dart

--- lib/ep1377_news_app/news_detail/view/news_app_detail_page.dart
+++ lib/ep1377_news_app/news_detail/view/news_app_detail_page.dart
@@ -94,9 +94,21 @@ class _NewsAppDetailPageState extends State<NewsAppDetailPage> {
                               fit: BoxFit.cover),
                         ),
                       ),
-                      Text("Star"),
+                      Text(
+                        "Star",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      SizedBox(height: 16,),
                       SelectableText(
-                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+                          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                        style: TextStyle(
+                          height: 1.2,
+                          fontWeight: FontWeight.w500
+                        ),
+                      )
                     ],
                   ),
                 ),

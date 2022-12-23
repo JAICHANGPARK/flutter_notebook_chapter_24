53760901bf6b4c46571b4efd96bf49c881fa61f4
Dreamwalker <aristojeff@gmail.com>
2022-12-23 21:42:53 +0900

    Update news_app_detail_page.dart

--- lib/ep1377_news_app/news_detail/view/news_app_detail_page.dart
+++ lib/ep1377_news_app/news_detail/view/news_app_detail_page.dart
@@ -12,22 +12,46 @@ class _NewsAppDetailPageState extends State<NewsAppDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  height: 48,
-                  width: 48,
-                  decoration: BoxDecoration(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 48,
+                    width: 48,
+                    decoration: BoxDecoration(
                       shape: BoxShape.circle,
                       border: Border.all(
                         color: Colors.grey,
-                      )),
-                )
-              ],
-            )
-          ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: Center(
+                    child: Text(
+                      "Sports",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18
+                      ),
+                    ),
+                  )),
+                  Container(
+                    height: 48,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

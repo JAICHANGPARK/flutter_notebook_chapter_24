1be5a9ba30435d416994eaad23922fc605fd62bb
Dreamwalker <aristojeff@gmail.com>
2022-12-23 21:09:28 +0900

    Update news_app_main_apge.dart

--- lib/ep1377_news_app/view/news_app_main_apge.dart
+++ lib/ep1377_news_app/view/news_app_main_apge.dart
@@ -22,199 +22,212 @@ class _NewsAppMainPageState extends State<NewsAppMainPage> {
               right: 0,
               bottom: 100,
               top: 16,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: IndexedStack(
+                index: _index,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(right: 16),
-                    child: Column(
-                      children: [
-                        Row(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Column(
                           children: [
-                            const CircleAvatar(
-                              radius: 28,
-                            ),
-                            const SizedBox(
-                              width: 12,
-                            ),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: const [
-                                Text(
-                                  "Welcome back!",
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                  ),
+                            Row(
+                              children: [
+                                const CircleAvatar(
+                                  radius: 28,
                                 ),
-                                SizedBox(
-                                  height: 8,
+                                const SizedBox(
+                                  width: 12,
                                 ),
-                                Text(
-                                  "Dreamwalker",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: const [
+                                    Text(
+                                      "Welcome back!",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      "Dreamwalker",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const Spacer(),
+                                Container(
+                                  height: 48,
+                                  width: 48,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(),
+                                    shape: BoxShape.circle,
                                   ),
+                                  child: const Icon(Ionicons.notifications_outline),
                                 )
                               ],
                             ),
-                            const Spacer(),
                             Container(
-                              height: 48,
-                              width: 48,
+                              margin: const EdgeInsets.symmetric(vertical: 24),
+                              height: 60,
                               decoration: BoxDecoration(
-                                border: Border.all(),
-                                shape: BoxShape.circle,
+                                color: Colors.grey[300],
+                                borderRadius: BorderRadius.circular(8),
                               ),
-                              child: const Icon(Ionicons.notifications_outline),
+                              padding: const EdgeInsets.symmetric(horizontal: 16),
+                              child: const Center(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    icon: Icon(Icons.search),
+                                    hintText: "Search",
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
                             )
                           ],
                         ),
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 24),
-                          height: 60,
-                          decoration: BoxDecoration(
-                            color: Colors.grey[300],
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          padding: const EdgeInsets.symmetric(horizontal: 16),
-                          child: const Center(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                icon: Icon(Icons.search),
-                                hintText: "Search",
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text(
-                            "Hot news",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                            ),
-                          ),
-                          Container(
-                            margin: const EdgeInsets.symmetric(vertical: 16),
-                            height: 64,
-                            child: Row(
-                              children: [
-                                Container(
-                                  height: 64,
-                                  width: 64,
-                                  margin: const EdgeInsets.only(right: 8),
-                                  decoration: BoxDecoration(
-                                    shape: BoxShape.circle,
-                                    color: Colors.grey[200],
-                                  ),
-                                  child: IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.add,
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  child: ListView.builder(
-                                    scrollDirection: Axis.horizontal,
-                                    itemCount: 10,
-                                    itemBuilder: (context, index) {
-                                      return Container(
-                                        height: 64,
-                                        width: 64,
-                                        margin: const EdgeInsets.only(right: 8),
-                                        decoration: const BoxDecoration(
-                                          shape: BoxShape.circle,
-                                          color: Colors.black,
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      ),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               const Text(
-                                "Popular",
+                                "Hot news",
                                 style: TextStyle(
                                   fontWeight: FontWeight.bold,
                                   fontSize: 24,
                                 ),
                               ),
-                              TextButton(
-                                onPressed: () {},
-                                style: TextButton.styleFrom(
-                                  foregroundColor: Colors.red,
+                              Container(
+                                margin: const EdgeInsets.symmetric(vertical: 16),
+                                height: 64,
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      height: 64,
+                                      width: 64,
+                                      margin: const EdgeInsets.only(right: 8),
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.grey[200],
+                                      ),
+                                      child: IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(
+                                          Icons.add,
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: 10,
+                                        itemBuilder: (context, index) {
+                                          return Container(
+                                            height: 64,
+                                            width: 64,
+                                            margin: const EdgeInsets.only(right: 8),
+                                            decoration: const BoxDecoration(
+                                              shape: BoxShape.circle,
+                                              color: Colors.black,
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    )
+                                  ],
                                 ),
-                                child: const Text("See All"),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text(
+                                    "Popular",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 24,
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    style: TextButton.styleFrom(
+                                      foregroundColor: Colors.red,
+                                    ),
+                                    child: const Text("See All"),
+                                  )
+                                ],
+                              ),
+                              SizedBox(
+                                height: 360,
+                                child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: 10,
+                                  itemBuilder: (context, index) {
+                                    return Container(
+                                      margin: const EdgeInsets.only(right: 16),
+                                      width: 300,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Expanded(
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.circular(8),
+                                              child: Image.network(
+                                                "https://cdn.pixabay.com/photo/2012/11/28/11/11/quarterback-67700_960_720.jpg",
+                                                fit: BoxFit.cover,
+                                                width: double.infinity,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 8,
+                                          ),
+                                          const Text(
+                                            "News Title News Title News Title News Title News Title News Title News Title",
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                            style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 18,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 8,
+                                          ),
+                                          const Text(
+                                            "Dream Walker - 3h ago",
+                                            style: TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.grey,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
                               )
                             ],
                           ),
-                          SizedBox(
-                            height: 360,
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 10,
-                              itemBuilder: (context, index) {
-                                return Container(
-                                  margin: const EdgeInsets.only(right: 16),
-                                  width: 300,
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Expanded(
-                                        child: ClipRRect(
-                                          borderRadius: BorderRadius.circular(8),
-                                          child: Image.network(
-                                            "https://cdn.pixabay.com/photo/2012/11/28/11/11/quarterback-67700_960_720.jpg",
-                                            fit: BoxFit.cover,
-                                            width: double.infinity,
-                                          ),
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 8,
-                                      ),
-                                      const Text(
-                                        "News Title News Title News Title News Title News Title News Title News Title",
-                                        maxLines: 2,
-                                        overflow: TextOverflow.ellipsis,
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 18,
-                                        ),
-                                      ),
-                                      const SizedBox(
-                                        height: 8,
-                                      ),
-                                      const Text(
-                                        "Dream Walker - 3h ago",
-                                        style: TextStyle(
-                                          fontSize: 12,
-                                          color: Colors.grey,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              },
-                            ),
-                          )
-                        ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
+                  Column(
+                    children: [
+                      Container(
+                        height: 52,
+                        color: Colors.brown,
+                      )
+                    ],
+                  )
                 ],
               ),
             ),

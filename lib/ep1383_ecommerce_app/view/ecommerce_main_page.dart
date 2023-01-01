c6e81d46d9b3fbdeda3d6170f4c8b3bf372960f0
Dreamwalker <aristojeff@gmail.com>
2023-01-01 11:04:34 +0900

    Update ecommerce_main_page.dart

--- lib/ep1383_ecommerce_app/view/ecommerce_main_page.dart
+++ lib/ep1383_ecommerce_app/view/ecommerce_main_page.dart
@@ -16,37 +16,42 @@ class _EcommerceMainPageState extends State<EcommerceMainPage> {
       body: SafeArea(
         child: IndexedStack(
           index: _menuIndex,
-          children: [],
+          children: [
+
+          ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Color(0xFFFF7800),
-        unselectedItemColor: Colors.grey,
-        currentIndex: _menuIndex,
-        onTap: (idx) {
-          setState(() {
-            _menuIndex = idx;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.thumb_up_alt_outlined),
-            label: "For you",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.wallet),
-            label: "Wallet",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.favorite),
-            label: "Collection",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: "Settings",
-          )
-        ],
+      bottomNavigationBar: SizedBox(
+        height: 80,
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: const Color(0xFFFF7800),
+          unselectedItemColor: Colors.grey,
+          currentIndex: _menuIndex,
+          onTap: (idx) {
+            setState(() {
+              _menuIndex = idx;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.thumb_up),
+              label: "For you",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.wallet),
+              label: "Wallet",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite),
+              label: "Collection",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: "Settings",
+            )
+          ],
+        ),
       ),
     );
   }

9fefe77b74fd10ef8564b79ba5f989e46f41d767
Dreamwalker <aristojeff@gmail.com>
2023-01-01 10:58:52 +0900

    Create ecommerce_home_widget.dart

---
+++ lib/ep1383_ecommerce_app/view/ecommerce_home_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class EcommerceHomeWidget extends StatefulWidget {
+  const EcommerceHomeWidget({Key? key}) : super(key: key);
+
+  @override
+  State<EcommerceHomeWidget> createState() => _EcommerceHomeWidgetState();
+}
+
+class _EcommerceHomeWidgetState extends State<EcommerceHomeWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}

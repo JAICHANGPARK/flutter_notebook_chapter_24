c01966cb177e501d16827ed9352a30fb06306a6f
Dreamwalker <aristojeff@gmail.com>
2023-01-01 10:55:47 +0900

    Create ep1383_main.dart

---
+++ lib/ep1383_ecommerce_app/ep1383_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+
+}
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

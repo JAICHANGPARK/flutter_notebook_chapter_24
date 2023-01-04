a8f6f9d7011a2e9270a7002718af9c5806451a5e
Dreamwalker <aristojeff@gmail.com>
2023-01-04 19:25:40 +0900

    Create almystic_main_page.dart

---
+++ lib/ep1386_ai_image_generator_app/view/almystic_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlmysticMainPage extends StatefulWidget {
+  const AlmysticMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<AlmysticMainPage> createState() => _AlmysticMainPageState();
+}
+
+class _AlmysticMainPageState extends State<AlmysticMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

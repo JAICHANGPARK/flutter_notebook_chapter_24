117c034ee4079a27fb4c279e5ac06491b056c408
Dreamwalker <aristojeff@gmail.com>
2023-01-04 19:24:36 +0900

    Create ep1386_main.dart

---
+++ lib/ep1386_ai_image_generator_app/ep1386_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class AIImageGeneratorApp extends StatelessWidget {
+  const AIImageGeneratorApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

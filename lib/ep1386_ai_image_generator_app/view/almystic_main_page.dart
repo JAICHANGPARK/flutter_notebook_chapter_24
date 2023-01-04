460ed1be0b4cf071912cd5c68cd8a5cc1a4eef6b
Dreamwalker <aristojeff@gmail.com>
2023-01-04 19:33:44 +0900

    Update almystic_main_page.dart

--- lib/ep1386_ai_image_generator_app/view/almystic_main_page.dart
+++ lib/ep1386_ai_image_generator_app/view/almystic_main_page.dart
@@ -11,17 +11,20 @@ class _AlmysticMainPageState extends State<AlmysticMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(26, 28, 34, 1),
       body: SafeArea(
         child: Column(
           children: [],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromRGBO(85, 99, 249, 1),
         onPressed: () {},
         child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
+        color: Color.fromRGBO(35, 36, 42, 1),
         child: Container(
           height: 72,
         ),

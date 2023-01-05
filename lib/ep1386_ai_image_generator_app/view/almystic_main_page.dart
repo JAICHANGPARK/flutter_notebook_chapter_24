a13d27ff3e373c8682e28124dddf1875a83359d7
Dreamwalker <aristojeff@gmail.com>
2023-01-05 17:29:30 +0900

    Update almystic_main_page.dart

--- lib/ep1386_ai_image_generator_app/view/almystic_main_page.dart
+++ lib/ep1386_ai_image_generator_app/view/almystic_main_page.dart
@@ -13,21 +13,24 @@ class _AlmysticMainPageState extends State<AlmysticMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromRGBO(26, 28, 34, 1),
+      backgroundColor: const Color.fromRGBO(26, 28, 34, 1),
       body: SafeArea(
         child: Column(
           children: [],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromRGBO(85, 99, 249, 1),
+        backgroundColor: const Color.fromRGBO(85, 99, 249, 1),
         onPressed: () {},
-        child: Icon(Icons.add, size: 32,),
+        child: const Icon(
+          Icons.add,
+          size: 32,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        color: Color.fromRGBO(35, 36, 42, 1),
-        shape: CircularNotchedRectangle(),
+        color: const Color.fromRGBO(35, 36, 42, 1),
+        shape: const CircularNotchedRectangle(),
         notchMargin: 8,
         child: Container(
           height: 80,
@@ -36,112 +39,112 @@ class _AlmysticMainPageState extends State<AlmysticMainPage> {
               Expanded(
                   flex: 2,
                   child: Column(
-                children: [
-                  Container(
-                    height: 3,
-                    width: 32,
-                    decoration: BoxDecoration(
-                      color: _index == 1 ? Colors.blue : Colors.transparent,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _index = 1;
-                      });
-                    },
-                    icon: Icon(Icons.home_outlined),
-                    color: _index == 1 ? Colors.blue : Colors.grey,
-                    iconSize: 32,
-                  )
-                ],
-              )),
+                    children: [
+                      Container(
+                        height: 3,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color: _index == 1 ? Colors.blue : Colors.transparent,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _index = 1;
+                          });
+                        },
+                        icon: const Icon(Icons.home_outlined),
+                        color: _index == 1 ? Colors.blue : Colors.grey,
+                        iconSize: 32,
+                      )
+                    ],
+                  )),
               Expanded(
                   flex: 2,
                   child: Column(
-                children: [
-                  Container(
-                    height: 3,
-                    width: 32,
-                    decoration: BoxDecoration(
-                      color: _index == 2 ? Colors.blue : Colors.transparent,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _index = 2;
-                      });
-                    },
-                    icon: Icon(Icons.explore_outlined),
-                    color: _index == 2 ? Colors.blue : Colors.grey,
-                    iconSize: 32,
-                  )
-                ],
-              )),
+                    children: [
+                      Container(
+                        height: 3,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color: _index == 2 ? Colors.blue : Colors.transparent,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _index = 2;
+                          });
+                        },
+                        icon: const Icon(Icons.explore_outlined),
+                        color: _index == 2 ? Colors.blue : Colors.grey,
+                        iconSize: 32,
+                      )
+                    ],
+                  )),
               Expanded(flex: 2, child: Container()),
               Expanded(
-                flex: 2,
+                  flex: 2,
                   child: Column(
-                children: [
-                  Container(
-                    height: 3,
-                    width: 32,
-                    decoration: BoxDecoration(
-                      color: _index == 3 ? Colors.blue : Colors.transparent,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _index = 3;
-                      });
-                    },
-                    icon: Icon(Icons.folder_copy_outlined),
-                    color: _index == 3 ? Colors.blue : Colors.grey,
-                    iconSize: 32,
-                  )
-                ],
-              )),
+                    children: [
+                      Container(
+                        height: 3,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color: _index == 3 ? Colors.blue : Colors.transparent,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _index = 3;
+                          });
+                        },
+                        icon: const Icon(Icons.folder_copy_outlined),
+                        color: _index == 3 ? Colors.blue : Colors.grey,
+                        iconSize: 32,
+                      )
+                    ],
+                  )),
               Expanded(
                   flex: 2,
                   child: Column(
-                children: [
-                  Container(
-                    height: 3,
-                    width: 32,
-                    decoration: BoxDecoration(
-                      color: _index == 4 ? Colors.blue : Colors.transparent,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      setState(() {
-                        _index = 4;
-                      });
-                    },
-                    icon: Icon(Icons.person_2_outlined),
-                    color: _index == 4 ? Colors.blue : Colors.grey,
-                    iconSize: 32,
-                  )
-                ],
-              )),
+                    children: [
+                      Container(
+                        height: 3,
+                        width: 32,
+                        decoration: BoxDecoration(
+                          color: _index == 4 ? Colors.blue : Colors.transparent,
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          setState(() {
+                            _index = 4;
+                          });
+                        },
+                        icon: const Icon(Icons.person_2_outlined),
+                        color: _index == 4 ? Colors.blue : Colors.grey,
+                        iconSize: 32,
+                      )
+                    ],
+                  )),
             ],
           ),
         ),

80d8fad5fb5d7aa321034fcb6e4285937b8baf72
Dreamwalker <aristojeff@gmail.com>
2023-01-05 17:35:59 +0900

    Update ep1386_main.dart

--- lib/ep1386_ai_image_generator_app/ep1386_main.dart
+++ lib/ep1386_ai_image_generator_app/ep1386_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1386_ai_image_generator_app/view/almystic_main_page.dart';
 
 void main() {
-  runApp(AIImageGeneratorApp());
+  runApp(const AIImageGeneratorApp());
 }
 
 class AIImageGeneratorApp extends StatelessWidget {
@@ -10,7 +10,7 @@ class AIImageGeneratorApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: AlmysticMainPage(),
     );
   }

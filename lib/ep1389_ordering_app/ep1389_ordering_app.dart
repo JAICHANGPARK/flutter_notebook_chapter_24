446fe04142cef5e4f3c368e651a28861872877ff
Dreamwalker <aristojeff@gmail.com>
2023-01-07 10:51:21 +0900

    Create ep1389_ordering_app.dart

---
+++ lib/ep1389_ordering_app/ep1389_ordering_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(OrderingApp());
+}
+
+class OrderingApp extends StatelessWidget {
+  const OrderingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}

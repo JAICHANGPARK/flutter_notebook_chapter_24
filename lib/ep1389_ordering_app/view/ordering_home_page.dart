74be90df7e21e9bbd11eae2361998cac291441c3
Dreamwalker <aristojeff@gmail.com>
2023-01-07 10:55:27 +0900

    Create ordering_home_page.dart

---
+++ lib/ep1389_ordering_app/view/ordering_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class OrderingHomePage extends StatefulWidget {
+  const OrderingHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<OrderingHomePage> createState() => _OrderingHomePageState();
+}
+
+class _OrderingHomePageState extends State<OrderingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}

5d629cd414c6879a33c6944d252b2aead46bc6ed
Dreamwalker <aristojeff@gmail.com>
2023-01-08 12:50:03 +0900

    Update ordering_home_page.dart

--- lib/ep1389_ordering_app/view/ordering_home_page.dart
+++ lib/ep1389_ordering_app/view/ordering_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class OrderingHomePage extends StatefulWidget {
   const OrderingHomePage({Key? key}) : super(key: key);
 
@@ -11,8 +10,56 @@ class OrderingHomePage extends StatefulWidget {
 class _OrderingHomePageState extends State<OrderingHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Column(children: [
+      Row(
+        children: [
+          Container(
+            height: 42,
+            width: 42,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(4),
+            ),
+            child: Icon(
+              Icons.location_on,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(
+            width: 16,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "My location",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "464 Royal Mesa, New Jersey",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          ),
+          Container(
+              height: 42,
+              width: 42,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(4),
+              ),
+              child: Center(
+                  child: Badge(
+                child: Icon(
+                  Icons.shopping_bag_outlined,
+                ),
+              ))),
+        ],
+      )
+    ]);
   }
 }

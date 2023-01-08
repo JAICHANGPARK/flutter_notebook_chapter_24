6dcc7515148babcdf8807c046c612afd60760582
Dreamwalker <aristojeff@gmail.com>
2023-01-08 12:51:01 +0900

    Update ordering_home_page.dart

--- lib/ep1389_ordering_app/view/ordering_home_page.dart
+++ lib/ep1389_ordering_app/view/ordering_home_page.dart
@@ -11,54 +11,60 @@ class _OrderingHomePageState extends State<OrderingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Row(
-        children: [
-          Container(
-            height: 42,
-            width: 42,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(4),
-            ),
-            child: Icon(
-              Icons.location_on,
-              color: Colors.white,
-            ),
-          ),
-          SizedBox(
-            width: 16,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "My location",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Container(
+              height: 42,
+              width: 42,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(4),
               ),
-              Text(
-                "464 Royal Mesa, New Jersey",
-                style: TextStyle(
-                  color: Colors.grey,
+              child: Icon(
+                Icons.location_on,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              width: 16,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "My location",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              )
-            ],
-          ),
-          Container(
+                Text(
+                  "464 Royal Mesa, New Jersey",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                )
+              ],
+            ),
+            const Spacer(),
+            Container(
               height: 42,
               width: 42,
               decoration: BoxDecoration(
                 color: Colors.grey[200],
                 borderRadius: BorderRadius.circular(4),
               ),
-              child: Center(
-                  child: Badge(
-                child: Icon(
-                  Icons.shopping_bag_outlined,
+              child: const Center(
+                child: Badge(
+                  child: Icon(
+                    Icons.shopping_bag_outlined,
+                  ),
                 ),
-              ))),
-        ],
+              ),
+            ),
+          ],
+        ),
       )
     ]);
   }

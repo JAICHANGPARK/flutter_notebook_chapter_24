de8352e784ed6a94f03eadfb841e3be42d600c39
Dreamwalker <aristojeff@gmail.com>
2023-01-09 17:16:19 +0900

    Update ordering_order_page.dart

--- lib/ep1389_ordering_app/view/ordering_order_page.dart
+++ lib/ep1389_ordering_app/view/ordering_order_page.dart
@@ -10,15 +10,36 @@ class OrderingOrderPage extends StatefulWidget {
 class _OrderingOrderPageState extends State<OrderingOrderPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          height: 52,
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 52,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+            ),
           ),
-        )
-      ],
+          SizedBox(height: 16,),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Your orders",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }

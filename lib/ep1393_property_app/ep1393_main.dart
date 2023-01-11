17a86b7751463822642126d3f10996228074b073
Dreamwalker <aristojeff@gmail.com>
2023-01-11 17:37:13 +0900

    Create ep1393_main.dart

---
+++ lib/ep1393_property_app/ep1393_main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+
+}
+
+
+class PropertyApp extends StatelessWidget {
+  const PropertyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

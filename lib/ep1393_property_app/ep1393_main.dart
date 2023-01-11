400b3c755c03293b09dd2747a7e5e65bdd7565c6
Dreamwalker <aristojeff@gmail.com>
2023-01-11 17:41:44 +0900

    Update ep1393_main.dart

--- lib/ep1393_property_app/ep1393_main.dart
+++ lib/ep1393_property_app/ep1393_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_24/ep1393_property_app/view/property_main_page.dart';
 
 void main() {
-
+  runApp(PropertyApp());
 }
 
 class PropertyApp extends StatelessWidget {

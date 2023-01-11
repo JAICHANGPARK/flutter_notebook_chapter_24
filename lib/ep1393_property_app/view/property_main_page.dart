a954f595a855fb6dc749fa9a4dce6ccfb4ec7c14
Dreamwalker <aristojeff@gmail.com>
2023-01-11 17:50:27 +0900

    Update property_main_page.dart

--- lib/ep1393_property_app/view/property_main_page.dart
+++ lib/ep1393_property_app/view/property_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PropertyMainPage extends StatefulWidget {
   const PropertyMainPage({Key? key}) : super(key: key);
@@ -11,7 +12,75 @@ class _PropertyMainPageState extends State<PropertyMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Home"),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.black,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {},
+          ),
+          actions: [Icon(Icons.notifications_none)],
+        ),
+        body: Column(
+          children: [
+            Container(
+                padding: EdgeInsets.fromLTRB(16, 24, 16, 32),
+                color: Colors.black,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Welcome Back!",
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 6,
+                    ),
+                    Text(
+                      "Dreamwalker",
+                      style: GoogleFonts.inter(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: 48,
+                      margin: EdgeInsets.only(
+                        top: 24,
+                      ),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Container(
+                            width: 48,
+                            height: 48,
+                            decoration: BoxDecoration(
+                              color: Color(0xfffa902e),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                )),
+            Row(
+              children: [],
+            )
+          ],
+        ));
   }
 }

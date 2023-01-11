584b79c43fe59f060d99ca655881c3a52532e3a3
Dreamwalker <aristojeff@gmail.com>
2023-01-11 17:39:26 +0900

    Create property_main_page.dart

---
+++ lib/ep1393_property_app/view/property_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PropertyMainPage extends StatefulWidget {
+  const PropertyMainPage({Key? key}) : super(key: key);
+
+  @override
+  State<PropertyMainPage> createState() => _PropertyMainPageState();
+}
+
+class _PropertyMainPageState extends State<PropertyMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

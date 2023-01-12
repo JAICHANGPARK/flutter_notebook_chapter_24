416867243656bfb1e04107df43baab1e1acf7064
Dreamwalker <aristojeff@gmail.com>
2023-01-12 16:55:45 +0900

    Update property_main_page.dart

--- lib/ep1393_property_app/view/property_main_page.dart
+++ lib/ep1393_property_app/view/property_main_page.dart
@@ -12,127 +12,157 @@ class _PropertyMainPageState extends State<PropertyMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Home"),
-          foregroundColor: Colors.white,
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: const Icon(Icons.menu),
-            onPressed: () {},
-          ),
-          actions: [const Icon(Icons.notifications_none)],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Home"),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {},
         ),
-        body: Column(
-          children: [
-            Container(
-                padding: const EdgeInsets.fromLTRB(16, 24, 16, 32),
-                color: Colors.black,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Welcome Back!",
-                      style: GoogleFonts.inter(
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 6,
-                    ),
-                    Text(
-                      "Dreamwalker",
-                      style: GoogleFonts.inter(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Container(
-                      height: 48,
-                      margin: const EdgeInsets.only(
-                        top: 24,
-                      ),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                              padding: const EdgeInsets.symmetric(horizontal: 16),
-                              child: const TextField(
-                                decoration: InputDecoration(
-                                  icon: Icon(Icons.search),
-                                  hintText: "Search Your Location",
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          Container(
-                            width: 48,
-                            height: 48,
-                            decoration: BoxDecoration(
-                              color: const Color(0xfffa902e),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: const Icon(
-                              Icons.tune,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                )),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16, 24, 16, 8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        actions: [const Icon(Icons.notifications_none)],
+      ),
+      body: Column(
+        children: [
+          Container(
+              padding: const EdgeInsets.fromLTRB(16, 24, 16, 32),
+              color: Colors.black,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    "Recent Posted",
+                    "Welcome Back!",
                     style: GoogleFonts.inter(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
+                      color: Colors.white,
                     ),
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.orange,
-                      textStyle: GoogleFonts.inter(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                      ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Text(
+                    "Dreamwalker",
+                    style: GoogleFonts.inter(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
                     ),
-                    child: Text(
-                      "View all",
+                  ),
+                  Container(
+                    height: 48,
+                    margin: const EdgeInsets.only(
+                      top: 24,
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: const EdgeInsets.symmetric(horizontal: 16),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                icon: Icon(Icons.search),
+                                hintText: "Search Your Location",
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        Container(
+                          width: 48,
+                          height: 48,
+                          decoration: BoxDecoration(
+                            color: const Color(0xfffa902e),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: const Icon(
+                            Icons.tune,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
                     ),
                   )
                 ],
-              ),
+              )),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 24, 16, 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Recent Posted",
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.orange,
+                    textStyle: GoogleFonts.inter(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                  child: Text(
+                    "View all",
+                  ),
+                )
+              ],
             ),
-            Container(
-              height: 200,
-              color: Colors.orange,
-              margin: EdgeInsets.only(left: 16),
-              child: ListView.builder(
-                  itemCount: 10,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      color: Colors.blue,
-                      width: 160,
-                    );
-                  }),
-            )
-          ],
-        ));
+          ),
+          Container(
+            height: 200,
+            color: Colors.orange,
+            margin: EdgeInsets.only(left: 16),
+            child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    color: Colors.blue,
+                    width: 160,
+                  );
+                }),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Popular Places",
+                  style: GoogleFonts.inter(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.orange,
+                    textStyle: GoogleFonts.inter(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                  child: Text(
+                    "View all",
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(child: Placeholder()),
+        ],
+      ),
+    );
   }
 }

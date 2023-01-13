4ab2a94b45e713ce17562109f4f3f7bc138ded1a
Dreamwalker <aristojeff@gmail.com>
2023-01-13 17:10:44 +0900

    Update property_search_page.dart

--- lib/ep1393_property_app/view/property_search_page.dart
+++ lib/ep1393_property_app/view/property_search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PropertySearchResultPage extends StatefulWidget {
   const PropertySearchResultPage({Key? key}) : super(key: key);
@@ -14,20 +15,39 @@ class _PropertySearchResultPageState extends State<PropertySearchResultPage> {
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-                  height: 52,
-                  width: 52,
-                  decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey[200]!,
-                      spreadRadius: 2,
-                      blurRadius: 2
-                    )
-                  ]),
-                )
-              ],
+            Container(
+              color: Colors.blue,
+              child: Stack(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 52,
+                        width: 52,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey[200]!,
+                              spreadRadius: 2,
+                              blurRadius: 2,
+                            )
+                          ],
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.arrow_back),
+                          onPressed: () {},
+                        ),
+                      ),
+
+                    ],
+                  ),
+                  Center(
+                    child: Text("Search Results",style: GoogleFonts.inter(),),
+                  )
+                ],
+              ),
             )
           ],
         ),

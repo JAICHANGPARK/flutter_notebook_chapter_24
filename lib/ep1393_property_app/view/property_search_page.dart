e1c6b397b2e7c420bed29afa9e0c8547843afadf
Dreamwalker <aristojeff@gmail.com>
2023-01-13 17:07:43 +0900

    Update property_search_page.dart

---
+++ lib/ep1393_property_app/view/property_search_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PropertySearchResultPage extends StatefulWidget {
+  const PropertySearchResultPage({Key? key}) : super(key: key);
+
+  @override
+  State<PropertySearchResultPage> createState() => _PropertySearchResultPageState();
+}
+
+class _PropertySearchResultPageState extends State<PropertySearchResultPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

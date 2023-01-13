15b5870dbe4243aef5aa0a77a6fff9577b61f0ee
Dreamwalker <aristojeff@gmail.com>
2023-01-13 17:12:46 +0900

    Update property_search_page.dart

--- lib/ep1393_property_app/view/property_search_page.dart
+++ lib/ep1393_property_app/view/property_search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class PropertySearchResultPage extends StatefulWidget {
@@ -36,8 +37,10 @@ class _PropertySearchResultPageState extends State<PropertySearchResultPage> {
                           ],
                         ),
                         child: IconButton(
-                          icon: Icon(Icons.arrow_back),
-                          onPressed: () {},
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
                         ),
                       ),
                     ],

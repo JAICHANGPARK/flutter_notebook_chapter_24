1a89a3ac41dcb311eaaa0d46a52b1228df0d1210
Dreamwalker <aristojeff@gmail.com>
2023-01-14 10:21:47 +0900

    Update ep1396_main.dart

--- lib/ep1396_guitar_lesson_app/ep1396_main.dart
+++ lib/ep1396_guitar_lesson_app/ep1396_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_24/ep1396_guitar_lesson_app/view/guitar_lesson_main_page.dart';
 
 void main() {
   runApp(GuitarLessonApp());
@@ -9,6 +10,8 @@ class GuitarLessonApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return const MaterialApp(
+      home: GuitarLessonMainPage(),
+    );
   }
 }

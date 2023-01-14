4f5b3e0a01608150fff2a73ccf88197811935c54
Dreamwalker <aristojeff@gmail.com>
2023-01-14 10:08:55 +0900

    Create ep1396_main.dart

---
+++ lib/ep1396_guitar_lesson_app/ep1396_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GuitarLessonApp());
+}
+
+class GuitarLessonApp extends StatelessWidget {
+  const GuitarLessonApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

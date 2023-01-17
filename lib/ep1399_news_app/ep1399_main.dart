4c82ebb8722863b69b28ad1ed001f6ddaa8aa4f7
Dreamwalker <aristojeff@gmail.com>
2023-01-17 18:15:47 +0900

    Create ep1399_main.dart

---
+++ lib/ep1399_news_app/ep1399_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class NewsApp2 extends StatelessWidget {
+  const NewsApp2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

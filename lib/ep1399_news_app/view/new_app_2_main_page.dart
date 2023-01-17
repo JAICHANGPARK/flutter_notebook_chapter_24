b974a93a6a44944c62f5e96da95afdd7a52c1f64
Dreamwalker <aristojeff@gmail.com>
2023-01-17 18:17:37 +0900

    Create new_app_2_main_page.dart

---
+++ lib/ep1399_news_app/view/new_app_2_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewsApp2MainPage extends StatefulWidget {
+  const NewsApp2MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<NewsApp2MainPage> createState() => _NewsApp2MainPageState();
+}
+
+class _NewsApp2MainPageState extends State<NewsApp2MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

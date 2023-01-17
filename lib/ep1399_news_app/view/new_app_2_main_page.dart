2ce2d0c433834533d6d6226a65e2c6e7bb135c2a
Dreamwalker <aristojeff@gmail.com>
2023-01-17 18:20:25 +0900

    Update new_app_2_main_page.dart

--- lib/ep1399_news_app/view/new_app_2_main_page.dart
+++ lib/ep1399_news_app/view/new_app_2_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class NewsApp2MainPage extends StatefulWidget {
   const NewsApp2MainPage({Key? key}) : super(key: key);
 
@@ -16,7 +15,27 @@ class _NewsApp2MainPageState extends State<NewsApp2MainPage> {
       body: SafeArea(
         child: Stack(
           children: [
-
+            Positioned(
+              left: 0,
+              bottom: 16,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 32,
+                    backgroundColor: Colors.white,
+                  ),
+                  CircleAvatar(
+                    radius: 32,
+                    backgroundColor: Colors.white.withOpacity(0.3),
+                  ),
+                  CircleAvatar(
+                    radius: 32,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),

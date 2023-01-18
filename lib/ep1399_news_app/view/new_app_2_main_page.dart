f590d19d00df14934de90963b421764cc51e7b08
Dreamwalker <aristojeff@gmail.com>
2023-01-18 23:20:16 +0900

    Update new_app_2_main_page.dart

--- lib/ep1399_news_app/view/new_app_2_main_page.dart
+++ lib/ep1399_news_app/view/new_app_2_main_page.dart
@@ -152,7 +152,26 @@ class _NewsApp2MainPageState extends State<NewsApp2MainPage> {
                                       ),
                                     ),
                                     Text(
-                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+                                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                                      style: TextStyle(
+                                        height: 1.5,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Row(
+                                      children: [
+                                        CircleAvatar(
+                                          child: Icon(Icons.thumb_up_alt_outlined),
+                                        ),
+                                        CircleAvatar(
+                                          child: Icon(Icons.bookmark_border),
+                                        ),
+                                        CircleAvatar(
+                                          child: Icon(Icons.share),
+                                        ),
+                                      ],
+                                    )
                                   ],
                                 ),
                               ),
